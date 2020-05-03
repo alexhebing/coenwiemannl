fc8160f69fa5ff1efd412c9c22f9ad1bdfa953ae
Alex Hebing <a.h.j.hebing@uu.nl>
2020-05-03 07:06:06 +0200

    Close #11

--- expo/admin.py
+++ expo/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from .models import Expo
 
 class ExpoAdmin(admin.ModelAdmin):
-    fields = ['expo_name', 'expo_startdate', 'expo_enddate', 'expo_info_l1', 'expo_publish']
+    fields = ['expo_name', 'expo_startdate', 'expo_enddate', 'expo_info_l1', 'expo_image', 'admin_thumbnail', 'expo_publish']
     list_display = ['expo_name', 'expo_startdate', 'expo_publish']
     list_filter = ['expo_publish']
+    readonly_fields = ('admin_thumbnail',)
 
 admin.site.register(Expo, ExpoAdmin)
--- expo/models.py
+++ expo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import datetime
+from django.conf import settings
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 class Expo(models.Model):
@@ -13,6 +15,9 @@ class Expo(models.Model):
     expo_info_l2 = models.TextField(blank=True, null=True, verbose_name='info_en')
     expo_publish = models.BooleanField(null=True, verbose_name='published?')
 
+    expo_image = models.FileField(null=True, verbose_name='image', upload_to='images',
+                                    help_text='Op de website komt het plaatje eruit te zien zoals hieronder (qua grootte)')
+
     class Meta:
         managed = False
         db_table = 'library_expo'
@@ -24,6 +29,25 @@ class Expo(models.Model):
             end = datetime.strftime(self.expo_enddate, dateformat)
             return "{} - {}".format(start, end)
         return start
+
+    # display an image thumbnail in the admin
+    def admin_thumbnail(self):
+        if self.expo_image:
+            image = self.get_image_filepath()
+            return mark_safe(
+                '<img src="{}{}" width="256px"" />'.format(
+                    settings.MEDIA_URL, image))
+        else:
+            return ''
+
+    def get_image_filepath(self):
+        if self.expo_image:
+            image = self.expo_image
+            if 'images' not in image.name:
+                if not image.name.startswith('/'):
+                   image = "/{}".format(image)
+                image = "images{}".format(image)
+            return image
 
     def __str__(self):
         return self.expo_name
